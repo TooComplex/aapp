cc142e7f869126363fb51ad37ad8bb4c6c75915c
TooComplex <hosterwang@hotmail.com>
2022-09-25 22:00:36 +0800

    pbi

---
+++ reactCard/.tmp/build/.tmp/precompile/visualPlugin.d.ts
@@ -0,0 +1,4 @@
+import powerbiVisualsApi from "powerbi-visuals-api";
+import IVisualPlugin = powerbiVisualsApi.visuals.plugins.IVisualPlugin;
+declare var reactCard39EDD43A68214068AF64B404372F0271_DEBUG: IVisualPlugin;
+export default reactCard39EDD43A68214068AF64B404372F0271_DEBUG;
